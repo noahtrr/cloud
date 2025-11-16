b076ed5e062396a99855c6bca61f9bdfc3ef5c07
Noah <noahtueremis@gmail.com>
2025-11-16 20:15:06 +0100

    Add DNS zone configuration and update firewall rules for Ansible server

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,11 +20,20 @@ variable "hcloud_token" {
 variable "ssh_token" {
   sensitive = true
 }
+variable "tld_zone" {
+  sensitive = true
+}
 
 # Defining Data for the Server deployments
 data hcloud_ssh_key "by_fingerprint" {
   fingerprint = var.ssh_token
 }
+
+# Defining the DNS Zone - use if you want to create the zone via terraform
+#resource "hcloud_zone" "primary_zone" {
+#  name = var.tld_zone
+#  mode = "primary"
+#}
 
 # Defining the firewall to be created
 resource "hcloud_firewall" "ansible_firewall" {
@@ -78,11 +87,25 @@ resource "hcloud_firewall" "ansible_firewall" {
 
 # Defining the servers to be created
 resource "hcloud_server" "ansible" {
-  count       = 1
   name        = "ansible"
   server_type = "cx23"
   image       = "ubuntu-24.04"
   location    = "nbg1"
   ssh_keys    = [data.hcloud_ssh_key.by_fingerprint.id]
   firewall_ids = [hcloud_firewall.ansible_firewall.id]
+}
+
+# Defining DNS Records
+resource "hcloud_zone_rrset" "ansible" {
+  zone = var.tld_zone #hcloud_zone.primary_zone.name - use if you want to create the zone via terraform
+  name = "ansible"
+  type = "A"
+
+  ttl = 3600
+
+  records = [
+    { value = hcloud_server.ansible.ipv4_address, comment = "Ansible Server" },
+  ]
+
+  change_protection = false
 }
